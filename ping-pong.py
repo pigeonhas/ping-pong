a98deeb9eba0ebe822741db4a1aeff7711be5b0a
pigeonhas <137184030+pigeonhas@users.noreply.github.com>
2023-06-21 13:35:49 +0300

    v.1.2.
    Статичный мяч, исправленное положение рокеток, рабочий счетчик очков

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,9 @@
 from pygame import *
 
+game = True
+l_score = 0
+r_score = 0
+
 class Gamesprite(sprite.Sprite):
     def __init__(self , player_speed , player_x , player_y , player_image , weight , height):
         sprite.Sprite.__init__(self)
@@ -28,23 +32,47 @@ class Player(Gamesprite):
         if keys[K_DOWN] and self.rect.y < 800:
             self.rect.y += self.speed
 
+class Enemy(Gamesprite):
+    def update(self):
+        global l_score
+        global r_score
+        if self.rect.x > 1400:
+            self.rect.y = 490
+            self.rect.x = 740
+            l_score += 1
+        elif self.rect.x < 10:
+            self.rect.y = 490
+            self.rect.x = 740
+            r_score += 1
+
+
 window = display.set_mode((1500 , 1000))
 display.set_caption('ping-pong')
-background = transform.scale(image.load('background.jpg') , (1500 , 1000))
-game = True
-rocket_l = Player(20 , 109 , 10 , 'racket.png' , 100 , 200)
-rocket_r = Player(20 , 1300 , 10 , 'racket.png' , 100 , 200)
 clock = time.Clock()
+
+background = transform.scale(image.load('background.jpg') , (1500 , 1000))
+rocket_l = Player(20 , 109 , 400 , 'racket.png' , 100 , 200)
+rocket_r = Player(20 , 1290 , 400 , 'racket.png' , 100 , 200)
+ball = Enemy(15 , 740 , 490 , 'tenis_ball.png' , 60 , 60 )
+
+font.init()
+font1 = font.SysFont('Bahnschrift' , 60 )
+
 while game:
+    text_l = font1.render(str(l_score) , 1 , (0 , 0 , 0))
+    text_r = font1.render(str(r_score) , 1 , (0 , 0 , 0))
+
     window.blit(background , (0 , 0))
+    window.blit(text_l , (10 , 10))
+    window.blit(text_r , (1460 , 10))
     rocket_l.update_l()
     rocket_r.update_r()
+    ball.update()
     rocket_r.reset()
     rocket_l.reset()
-    
+    ball.reset()
     for e in event.get():
         if e.type == QUIT:
-            finish = True
             game = False
 
     clock.tick(80)
